3db5e6bbf0fe26c6591656a430ef1b4f9465ef4c
Trabelsi Hassen <89588242+hassenok@users.noreply.github.com>
2022-11-27 15:35:27 +0100

    Add files via upload

---
+++ test/calculatePricaTest.java
@@ -0,0 +1,15 @@
+import com.c3.swe_automat.entitys.database.Bank;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assumptions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/*public class calculatePricaTest {
+    Bank bobj1;
+    @BeforeEach
+    void ini(){
+        bobj1=new Bank("")
+    }
+
+}*/
